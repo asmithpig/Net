4ca614ff58907c09e7dc019df1858c82ebf54185
xubinzha <xubinzha@usc.edu>
2024-03-07 19:00:49 -0600

    update

---
+++ Infrastructure/Data/EShopDbContext.cs
@@ -0,0 +1,13 @@
+using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class EShopDbContext: DbContext
+{
+    public EShopDbContext(DbContextOptions<EShopDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Customer> Customers { get; set; }
+}
