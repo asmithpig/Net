cc22c99ac34e722d39b5d269c91bb719ce4e58c4
xubinzha <xubinzha@usc.edu>
2024-03-11 01:32:45 -0500

    init

--- ApplicationCore/Entities/Product.cs
+++ ApplicationCore/Entities/Product.cs
@@ -10,6 +10,15 @@ public class Product
     public int Id { get; set; }
     
     [Required]
+    [Column(TypeName = "varchar(40")]
     public string ProductName { get; set; }
+    
+    public int? CategoryId { get; set; }
+    
+    public decimal? UnitPrice { get; set; }
+    
+    public short? UnitsInStock { get; set; }
+
+    public short? UnitsOnOrder { get; set; }
     
 }
