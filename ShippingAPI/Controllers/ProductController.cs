f7a6a689cf578d2c8837681858327422c24ba731
xubinzha <xubinzha@usc.edu>
2024-03-17 19:03:41 -0500

    api

---
+++ ShippingAPI/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+using ApplicationCore.Contracts.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShippingAPI.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    
+    private readonly IProductServiceAsync _productServiceAsync;
+
+    public ProductController(IProductServiceAsync productServiceAsync)
+    {
+        _productServiceAsync = productServiceAsync;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Products()
+    {
+        var products = await _productServiceAsync.GetAllProductsAsync();
+
+        if (!products.Any())
+        {
+            return NotFound(new { errorMessage = "No Products Found" }); 
+        }
+
+        return Ok(products);
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<IActionResult> Products(int id)
+    {
+        var product = await _productServiceAsync.GetProductByIdAsync(id);
+
+        if (product == null)
+        {
+            return NotFound(new { errorMessage = "No Product Found for id: " + id });
+        }
+        return Ok(product);
+    }
+}
